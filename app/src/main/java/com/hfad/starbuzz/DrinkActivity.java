9ef6b066f69f1664421484417f8c8d4029155257
gm-rion <gm-rion@mail.ru>
2019-11-03 13:42:35 +0300

    kuku

---
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -0,0 +1,34 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKID = "drinkId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        //Заполнение названия напитка
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        //Заполнение описания напитка
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        //Заполнение изображения напитка
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+    }
+}
